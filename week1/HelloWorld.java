a9ee143696fa7a7d7a43b55e2a7d2808fa81db8d
michaeldmora <moram1@go.stockton.edu>
2022-09-29 18:02:06 -0400

    Commit

--- week1/HelloWorld.java
+++ week1/HelloWorld.java
@@ -5,7 +5,10 @@ public class HelloWorld {
 public static void main(String[] args){
 
     System.out.println("Hello World!");
-    System.out.println("Today is thursday");
+    System.out.println("Today is thursday");    
+
+
+
 }
 
 }
