6a5d4dcf7873718f248bd35c958cc25fc8640f66
michaeldmora <moram1@go.stockton.edu>
2022-11-15 10:50:54 -0500

    test

---
+++ idk.java
@@ -0,0 +1,17 @@
+public class idk {
+    public static void main(String[] args) {
+        double s = 0.9;
+        double f = 0.1;
+
+        System.out.println("SSS= " + (s*s*s));
+        System.out.println("SSF= " + (s*s*f));
+        System.out.println("SFS= " + (s*f*s));
+        System.out.println("SFF= " + (s*f*f));
+        System.out.println("FSS= " + (f*s*s));
+        System.out.println("FSF= " + (f*s*f));
+        System.out.println("FFS= " + (f*f*s));
+        System.out.println("FFF= " + (f*f*f));
+        
+    }
+    
+}
